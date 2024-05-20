0ea8921766cdedd904d8d98b9c2ef6b874bc732a
Guilherme Ruiz <guilherme.ruiz@gmail.com>
2024-05-20 05:21:01 +0200

    configuring rustaceanvim and RustLsp mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<F4>", "<ESC><cmd>lua require('dap').toggle_breakpoint()<CR>", { desc = "DAP breakpoint" })
+map("n", "<F5>", "<ESC><cmd>lua require('dap').step_over()<CR>", { desc = "DAP step over" })
+map("n", "<F6>", "<ESC><cmd>lua require('dap').step_into()<CR>", { desc = "DAP step into" })
+map("n", "<F8>", "<ESC><cmd>lua require('dap').repl.open()<CR>", { desc = "DAP repl" })
+map("n", "<F9>", "<ESC><cmd>lua require('dap').continue()<CR>", { desc = "DAP continue" })
+map("n", "<F37>", "<cmd>RustLsp hover actions<CR>", { desc = "RustLsp actions" })
+map("n", "<F38>", "<cmd>RustLsp renderDiagnostic<CR>", { desc = "RustLsp diagnostic" })
+map("n", "<F39>", "<cmd>RustLsp explainError<CR>", { desc = "RustLsp explain" })
+map("n", "<F40>", "<cmd>RustLsp ssr<CR>", { desc = "RustLsp structural search and replace" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,32 +7,60 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "rust-analyzer",
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
+    "mrcjkb/rustaceanvim",
+    config = function ()
+      vim.g.rustaceanvim = {
+        tools = {
+          float_win_config = {
+            auto_focus = true
+          }
+        }
+      }
+    end,
+    version = "^4",
+    ft = { "rust" },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
 }
